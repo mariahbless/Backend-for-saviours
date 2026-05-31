63072f3a75b5e2cf614c48b0d188e051531c6c71
Gorret Golden <nabatanzigorret143@gmail.com>
2026-05-31 18:49:27 +0300

    Fix tests: add missing initials() method to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,16 @@ class User extends Authenticatable implements FilamentUser
     {
         return $this->hasMany(\App\Models\Loan::class);
     }
+
+    /**
+     * Get the user's initials
+     */
+    public function initials()
+    {
+        $words = explode(' ', $this->name);
+        if (count($words) >= 2) {
+            return strtoupper(substr($words[0], 0, 1) . substr($words[count($words) - 1], 0, 1));
+        }
+        return strtoupper(substr($this->name, 0, 2));
+    }
 }
